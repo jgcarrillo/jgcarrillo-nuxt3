a0ffe62649aa77c4f0da15a199cc95b1ffe3c4bf
Jorge G <jorgegomezweb@gmail.com>
2022-11-23 09:28:38 +0100

    fix: add slots to projects layout

--- layouts/projects.vue
+++ layouts/projects.vue
@@ -2,26 +2,51 @@
   <div
     class="my-10 mx-10 md:my-20 md:mx-16 lg:my-20 lg:mx-20 xl:my-20 xl:mx-60 2xl:my-20 2xl:mx-96"
   >
+    <!-- Arrow icon -->
     <div class="flex items-center justify-center">
       <NuxtLink to="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-12 h-12 text-gray-600 hover:text-gray-400 transition duration-500"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
-          />
-        </svg>
+        <IconsArrowIcon />
       </NuxtLink>
     </div>
 
-    <slot />
+    <!-- Image with title and year -->
+    <div class="relative">
+      <slot name="image" />
+      <div
+        class="flex flex-col justify-center items-center absolute w-full h-full top-0 right-0 text-white"
+      >
+        <h1 class="text-normal md:text-3xl font-bold mb-2">
+          <slot name="title"></slot>
+        </h1>
+        <h2 class="text-normal md:text-2xl font-thin">
+          <slot name="year"></slot>
+        </h2>
+      </div>
+    </div>
+
+    <!-- Content -->
+    <div>
+      <h2
+        class="text-2xl md:text-2xl text-center font-bold underline underline-offset-4 md:underline-offset-8 mb-14 md:mb-20"
+      >
+        Description
+      </h2>
+
+      <!-- Main content (text) -->
+      <slot />
+
+      <div class="my-20">
+        <h2
+          class="text-2xl md:text-2xl text-center font-bold underline underline-offset-4 md:underline-offset-8 mb-14 md:mb-20"
+        >
+          Tech used
+        </h2>
+        <div class="flex flex-row flex-wrap justify-center gap-2">
+          <!-- Tags (technologies) -->
+          <slot name="tags"></slot>
+        </div>
+      </div>
+    </div>
 
     <TheFooter />
   </div>
--- pages/projects/telegram-bot.vue
+++ pages/projects/telegram-bot.vue
@@ -1,97 +1,79 @@
 <template>
   <div>
-    <div class="relative">
-      <img
-        src="https://i.picsum.photos/id/347/5000/3334.jpg?hmac=y6ISvladvZWX6orqcoTW8rQ8XI0UPsptHbLbHooT57Q"
-        alt="side project image"
-        class="md:w-8/12 m-auto my-20 contrast-50"
-      />
-      <div
-        class="flex flex-col justify-center items-center absolute w-full h-full top-0 right-0 text-white"
-      >
-        <h1 class="text-normal md:text-3xl font-bold mb-2">Telegram Bot</h1>
-        <h2 class="text-normal md:text-2xl font-thin">2022</h2>
-      </div>
-    </div>
+    <NuxtLayout name="projects">
+      <template #image>
+        <img
+          src="https://i.picsum.photos/id/347/5000/3334.jpg?hmac=y6ISvladvZWX6orqcoTW8rQ8XI0UPsptHbLbHooT57Q"
+          alt="side project image"
+          class="md:w-8/12 m-auto my-20 contrast-50"
+        />
+      </template>
 
-    <h2
-      class="text-2xl md:text-2xl text-center font-bold underline underline-offset-4 md:underline-offset-8 mb-14 md:mb-20"
-    >
-      Description
-    </h2>
-    <p class="leading-relaxed mb-6">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-      magni, reprehenderit porro eius debitis laboriosam excepturi nobis
-      mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
-      accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Est sint unde minima amet voluptatum nobis mollitia
-      porro sequi dolor! Sit rerum tenetur eaque possimus delectus quidem
-      officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
-      nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
-      possimus! Tempore minus debitis ea eligendi qui.
-    </p>
+      <template #title>Telegram Bot</template>
+      <template #year>2022</template>
 
-    <p class="leading-relaxed mb-6">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-      magni, reprehenderit porro eius debitis laboriosam excepturi nobis
-      mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
-      accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Est sint unde minima amet voluptatum nobis mollitia
-      porro sequi dolor! Sit rerum tenetur eaque possimus delectus quidem
-      officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
-      nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
-      possimus! Tempore minus debitis ea eligendi qui.
-    </p>
+      <p class="leading-relaxed mb-6">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+        magni, reprehenderit porro eius debitis laboriosam excepturi nobis
+        mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
+        accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Est sint unde minima amet voluptatum nobis
+        mollitia porro sequi dolor! Sit rerum tenetur eaque possimus delectus
+        quidem officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
+        nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
+        possimus! Tempore minus debitis ea eligendi qui.
+      </p>
 
-    <p class="leading-relaxed mb-6">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-      magni, reprehenderit porro eius debitis laboriosam excepturi nobis
-      mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
-      accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Est sint unde minima amet voluptatum nobis mollitia
-      porro sequi dolor! Sit rerum tenetur eaque possimus delectus quidem
-      officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
-      nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
-      possimus! Tempore minus debitis ea eligendi qui.
-    </p>
+      <p class="leading-relaxed mb-6">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+        magni, reprehenderit porro eius debitis laboriosam excepturi nobis
+        mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
+        accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Est sint unde minima amet voluptatum nobis
+        mollitia porro sequi dolor! Sit rerum tenetur eaque possimus delectus
+        quidem officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
+        nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
+        possimus! Tempore minus debitis ea eligendi qui.
+      </p>
 
-    <div class="my-20">
-      <h2
-        class="text-2xl md:text-2xl text-center font-bold underline underline-offset-4 md:underline-offset-8 mb-14 md:mb-20"
-      >
-        Tech used
-      </h2>
-      <div class="flex flex-row flex-wrap justify-center gap-2">
-        <p
-          class="text-sm lg:text-lg p-1 rounded-xl md:m-1 md:py-1 md:px-4 md:rounded-full text-white bg-brand"
-        >
-          #TypeScript
-        </p>
-        <p
-          class="text-sm lg:text-lg p-1 rounded-xl md:m-1 md:py-1 md:px-4 md:rounded-full text-white bg-brand"
-        >
-          #grammY
-        </p>
-        <p
-          class="text-sm lg:text-lg p-1 rounded-xl md:m-1 md:py-1 md:px-4 md:rounded-full text-white bg-brand"
-        >
-          #Axios
-        </p>
-        <p
-          class="text-sm lg:text-lg p-1 rounded-xl md:m-1 md:py-1 md:px-4 md:rounded-full text-white bg-brand"
-        >
-          #Vercel
-        </p>
-      </div>
-    </div>
+      <p class="leading-relaxed mb-6">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+        magni, reprehenderit porro eius debitis laboriosam excepturi nobis
+        mollitia nesciunt. Aliquid, tempore voluptatem. Veniam adipisci odit
+        accusamus esse iste quibusdam quod! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Est sint unde minima amet voluptatum nobis
+        mollitia porro sequi dolor! Sit rerum tenetur eaque possimus delectus
+        quidem officiis vitae impedit accusantium. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Ullam amet, ipsa ab exercitationem dicta,
+        nihil quibusdam itaque saepe recusandae doloremque vero enim debitis
+        possimus! Tempore minus debitis ea eligendi qui.
+      </p>
+
+      <template #tags>
+        <div v-for="tag in tags" :key="tag">
+          <p
+            class="text-sm lg:text-lg p-1 rounded-xl md:m-1 md:py-1 md:px-4 md:rounded-full text-white bg-brand"
+          >
+            {{ tag }}
+          </p>
+        </div>
+      </template>
+    </NuxtLayout>
   </div>
 </template>
 
 <script>
 definePageMeta({
-  layout: "projects",
+  layout: "false",
 });
+
+export default {
+  data() {
+    return {
+      tags: ["#TypeScript", "#grammY", "#Axios", "#Vercel"],
+    };
+  },
+};
 </script>
