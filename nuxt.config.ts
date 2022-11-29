a4459b97afc9f3eea14e6c789237ee548a30f9c1
Jorge G <jorgegomezweb@gmail.com>
2022-11-29 13:15:47 +0100

    feat: add thumbnail and zapp-studio experience

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,17 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Hi, I'm Jorge, welcome to my website! I'm a software developer based in Murcia (Spain).",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "/thumb.jpg",
+        },
         { name: "format-detection", content: "telephone=no" },
       ],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
